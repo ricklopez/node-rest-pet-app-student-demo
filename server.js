a4594016ded784e07960b4285b9b5d2952217cc5
rick <rick@ricklopez.io>
2019-12-13 22:12:24 -0500

    Add Express server with GET POST and PUT endpoints

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-console.log("Hello From Node");
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3001;
+const knex = require('./knex/knex.js');
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+app.get('/users', (req, res) => {
+  knex.from('users')
+  .select('first_name', 'last_name')
+  .then((items) => {
+    res.json(items);
+  });
+});
+
+app.post('/users', (req, res) => {
+  console.log(req.body);
+  knex('users')
+    .insert(req.body)
+    .returning('id')
+    .then((item) => {
+      res.json(item); 
+  })
+});
+
+app.put('/users', (req, res) => {
+  knex('users')
+  .where({ id: 4 })
+  .update({ last_name: 'lopez'})
+  .then((item) => {
+    res.json('ok');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('Server Runnin on PORT' + PORT);
+});
