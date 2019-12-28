e5ab4f379565b510d4c1d9cab2904b40cac14f5e
rick <rick@ricklopez.io>
2019-12-27 23:01:59 -0500

    Refactor to include dedicated routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3001;
-const knex = require('./knex/knex.js');
+const userRouter = require('./routes/users.routes');
 const app = express();
 
 // Middleware
@@ -9,33 +9,11 @@ app.use(bodyParser.json());
 
 
 // Routes
-app.get('/users', (req, res) => {
-  knex.from('users')
-  .select('id','first_name', 'last_name')
-  .then((items) => {
-    res.json(items);
-  });
-});
 
-app.post('/users', (req, res) => {
-  console.log(req.body);
-  knex('users')
-    .insert(req.body)
-    .returning('id')
-    .then((item) => {
-      res.json(item); 
-  })
-});
+// User.Routes
+app.use("/users",userRouter);
 
-app.put('/users/:userid', (req, res) => {
-  console.log(req.body, req.params.userid);
-  knex('users')
-  .where({ id: req.params.userid})
-  .update({first_name: req.body.first_name, last_name: req.body.last_name})
-  .then((item) => {
-    res.json('ok');
-  });
-});
+
 
 app.listen(PORT, () => {
   console.log('Server Running on PORT: ' + PORT);
